65fd9ea0a3aaa05df9b95f1629aa767f8a7938f0
Yasmim Oliveira <101141180+Yasmim2003@users.noreply.github.com>
2023-08-06 17:32:05 -0300

    Update eighth_question.asm

--- eighth_question.asm
+++ eighth_question.asm
@@ -1,9 +1,9 @@
 addi x17, x0, 0
 
 loop:
-	lb x12, 1025(x0)
-	addi x17, x17, 1
-	beq x12, x0, sair
+    lb x12, 1025(x0)
+    addi x17, x17, 1
+    beq x12, x0, sair
 analise:
     addi x13, x0, 1
     beq x17, x13, bit_1
@@ -11,11 +11,10 @@ analise:
     beq x17, x13, bit_2
     addi x13, x0, 3
     beq x17, x13, bit_3
-	addi x13, x0, 4
+    addi x13, x0, 4
     beq x13, x17, bit_4
     addi x17, x0, 0
     beq x0, x0, sair
-# falta alocar os valores
 # multiplica e soma para alocar
 sair:
-	halt 
+   halt 
